c5f3f3b596f70bf0f0f342a8b1f586ee6bf17f04
denispalchuk <palchukdenis@gmail.com>
2014-10-22 20:37:58 +0400

    create new DB, first test and mapping DB to java Objects

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -3,14 +3,28 @@ package com.epam.brest.courses.dao;
 /**
  * Created by denis on 10/20/14.
  */
-import com.epam.brest.courses.domain.User;
 
+import com.epam.brest.courses.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+
 public class UserDaoImpl implements UserDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @Override
     public List<User> getUsers() {
-        return null;
+        return jdbcTemplate.query("select userid, login, name from USER",new UserMapper());
     }
 
     @Override
@@ -20,5 +34,16 @@ public class UserDaoImpl implements UserDao{
 
     public void addUser(User user) {
 
+    }
+
+    public class UserMapper implements RowMapper<User> {
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+            User user = new User();
+            user.setUserId(resultSet.getLong("userid"));
+            user.setUserName(resultSet.getString("name"));
+            user.setLogin(resultSet.getString("login"));
+            return user;
+        }
     }
 }
--- app-dao/src/test/java/com/epam/brest/courses/dao/UserDaoImplTest.java
+++ app-dao/src/test/java/com/epam/brest/courses/dao/UserDaoImplTest.java
@@ -1,10 +1,26 @@
 package com.epam.brest.courses.dao;
+import com.epam.brest.courses.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+import java.util.List;
+
 /**
  * Created by denis on 10/22/14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/testApplicationContextSpring.xml"})
 public class UserDaoImplTest {
     @Autowired
     private UserDao userDao;
 
+    @Test
+    public void getUsers() {
+        List<User> users=userDao.getUsers();
+        assertNotNull(users);
+    }
 }
