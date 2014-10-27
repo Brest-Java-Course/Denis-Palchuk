9dc30d460a5b25b304d0a7a71838e1b9140ffcd3
denispalchuk <palchukdenis@gmail.com>
2014-10-27 21:16:41 +0400

    Create initializer class

---
+++ app-service/src/test/java/com/epam/brest/courses/service/UserDataFixture.java
@@ -0,0 +1,24 @@
+package com.epam.brest.courses.service;
+
+import com.epam.brest.courses.domain.User;
+
+/**
+ * Created by denis on 10/27/14.
+ */
+public class UserDataFixture {
+
+    public static User getNewUser() {
+        User user =new User();
+        user.setUserName("name");
+        user.setLogin("login");
+        return user;
+    }
+
+    public static User getExistUser(long id) {
+        User user =new User();
+        user.setUserName("name");
+        user.setLogin("login");
+        user.setUserId(id);
+        return user;
+    }
+}
