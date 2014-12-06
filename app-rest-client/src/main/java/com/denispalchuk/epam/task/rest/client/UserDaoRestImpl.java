993533330fde352ca32a64fed6e95078acf75459
denis <palchukdenis@gmail.com>
2014-12-06 21:55:23 +0300

    Add rest-client without tests

--- app-rest-client/src/main/java/com/denispalchuk/epam/task/rest/client/UserDaoRestImpl.java
+++ app-rest-client/src/main/java/com/denispalchuk/epam/task/rest/client/UserDaoRestImpl.java
@@ -13,57 +13,53 @@ import java.util.List;
  * Created by denis on 12/5/14.
  */
 public class UserDaoRestImpl implements UserDao {
+
     private static final Logger LOGGER = LogManager.getLogger();
     RestTemplate restTemplate = new RestTemplate();
     private static final String REST_URL="http://localhost:8080/users/";
 
-    @Override
     public List<User> getAllUsers() {
         LOGGER.debug("client request get all users");
         List<User> users=restTemplate.getForObject(REST_URL,List.class);
         return users;
     }
 
-    @Override
     public User getUserById(Long userId) {
         LOGGER.debug("client request user by id {}",userId);
-        User user=restTemplate.getForObject(REST_URL+"id/"+userId,User.class);
+        User user=restTemplate.getForObject(REST_URL+"id/{id}",User.class,userId);
         return user;
     }
 
-    @Override
     public User getUserByLogin(String userLogin) {
         LOGGER.debug("client request user by login {}",userLogin);
-        User user=restTemplate.getForObject(REST_URL+"login/"+userLogin,User.class);
+        User user=restTemplate.getForObject(REST_URL+"login/{userLogin}",User.class,userLogin);
         return user;
     }
 
-    @Override
     public Long addUser(User user) {
         Long id = restTemplate.postForObject(REST_URL, user, Long.class);
         LOGGER.debug("client request to add user {} and return id {}",user,id);
         return id;
     }
 
-    @Override
     public void removeUser(Long userId) {
         LOGGER.debug("client request to delete user with id {}",userId);
-        restTemplate.delete(REST_URL+"id/"+userId);
+        restTemplate.delete(REST_URL+"id/{userId}",userId);
     }
 
-    @Override
     public void updateUser(User user) {
         LOGGER.debug("client request to update user with id {}",user.getUserId());
         restTemplate.put(REST_URL,User.class);
     }
 
-    @Override
     public Integer getAverageAgeUsersWhoMessagedWithUser(Long userId) {
-        return null;
+        LOGGER.debug("client request to get average aga users who messaged with user with userid = {}",userId);
+        return restTemplate.getForObject(REST_URL+"avage?userId={userId}",Integer.class,userId);
     }
 
-    @Override
     public List<Message> getAllMessageFromUser(Long userId) {
-        return null;
+        LOGGER.debug("client request to get all message from user with userid = {}",userId);
+        List<Message> messages=restTemplate.getForObject(REST_URL+"?userId={userId}",List.class,userId);
+        return messages;
     }
 }
